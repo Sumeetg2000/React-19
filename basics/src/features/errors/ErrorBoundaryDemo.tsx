cb93bb72b406a635cf85bc8e6c35eca8ab50ef76
Sumeet <sumeet.g@simformsolutions.com>
2026-04-01 15:57:58 +0530

    fix: error boundry demo

--- basics/src/features/errors/ErrorBoundaryDemo.tsx
+++ basics/src/features/errors/ErrorBoundaryDemo.tsx
@@ -32,41 +32,46 @@ class ErrorBoundary extends Component<
 
 /* ---------- CRASH COMPONENT ---------- */
 
-function Crash({ trigger }: { trigger: boolean }) {
-  if (trigger) {
+function Crash() {
+  const [crash, setCrash] = useState(false);
+
+  if (crash) {
     throw new Error("Crash!");
   }
 
-  return <div className="text-sm">No error</div>;
+  return (
+    <>
+      <h3>Clicking the button will break the page</h3>
+      <div className="pt-4">
+        <button
+          onClick={() => setCrash(true)}
+          className="px-3 py-1 bg-red-500 text-white rounded"
+        >
+          Trigger Error
+        </button>
+      </div>
+    </>
+  );
 }
 
 /* ---------- MAIN ---------- */
 
 export default function ErrorBoundaryDemo() {
-  const [crash, setCrash] = useState(false);
-
   return (
     <DemoSection
       title="Error Boundary"
       description="Prevents app from crashing on runtime errors"
     >
-      <button
-        onClick={() => setCrash(true)}
-        className="px-3 py-1 bg-red-500 text-white rounded"
-      >
-        Trigger Error
-      </button>
-
       <div className="grid grid-cols-2 gap-4 mt-4">
         {/* WITHOUT */}
         <ComparePanel title="Without Error Boundary">
-          <Crash trigger={crash} />
+          <Crash />
         </ComparePanel>
 
         {/* WITH */}
         <ComparePanel title="With Error Boundary" color="green">
           <ErrorBoundary>
-            <Crash trigger={crash} />
+            <Crash />
           </ErrorBoundary>
         </ComparePanel>
       </div>
