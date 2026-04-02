9396f221d15c0f80e3bba80ed1b755bfd8344aa9
Sumeet <sumeet.g@simformsolutions.com>
2026-04-02 12:05:35 +0530

    feat: inital setup

--- basics/src/shared/utils/heavyCompute.ts
+++
@@ -1,7 +0,0 @@
-export function heavyCompute(input: string): string[] {
-  const result: string[] = [];
-  for (let i = 0; i < 20000; i++) {
-    result.push(`${input}-${i}`);
-  }
-  return result;
-}
